a414c994797832726a598bd7ba34b469c0ad10b5
최준영 <dnduddl2015@gmail.com>
2022-10-16 17:14:26 +0900

    Repository 구현(Users 등 models 컨트롤 하는 CRUD 구현)

--- repositories/post.repository.js
+++ repositories/post.repository.js
@@ -1,11 +1,33 @@
 const { Posts } = require('../models');
 
 class PostsRepository{
-    // findOneByPostId
-    // findAllPosts
-    // createPosts
-    // deletePosts
-    // updatePosts
+    findAllPosts = async(userId) => {
+        return await Posts.find();
+    }
+    findOneByUserId = async(userId) => {
+        return await Posts.find({where : {postId: postId}});
+    };
+    createPost = async(userId, imageUrl, title) => {
+        await Posts.create({
+            userId,
+            title,
+            imageUrl,
+            likesNum: Math.round(Math.random()*10),
+            commentsNum: Math.round(Math.random()*10)
+        });
+        return {msg: "Posts created"}
+    };
+    updatePost = async(userId, postId, title, imageUrl) => {
+        await Posts.update(
+            {where: {userId : userId, postId: postId}},
+            {title : title, imageUrl: imageUrl} 
+        )
+        return {msg: "Post updated"};
+    };
+    deletePost = async(postId) => {
+        await Posts.delete({where: {postId: postId}});
+        return {msg: "Post deleted"};
+    };
 }
 
 module.exports = PostsRepository;
